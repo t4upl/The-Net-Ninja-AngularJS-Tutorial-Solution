a906e8a03a2e316358577893716c7cf53ad2eeec
Patryk D <tomr33d32@gmail.com>
2018-12-13 16:54:32 +0100

    Step 11 finished.

--- app/app.js
+++ app/app.js
@@ -2,7 +2,29 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 
 myNinjaApp.controller('myNinjaController', ['$scope', function($scope){
   $scope.message = "Hello from controller";
-  $scope.ninjas = ['yoshi', 'ryu'];
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50
+    },
+    {
+      name: "Crytstal",
+      belt: "green",
+      rate: 30
+
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 51000
+    }
+  ];
 }]);
 
 myNinjaApp.config(function (){
