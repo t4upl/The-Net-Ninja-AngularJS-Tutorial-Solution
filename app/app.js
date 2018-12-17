261274fa959737c8378d12f0ffe37e82ead5bd1e
Patryk D <tomr33d32@gmail.com>
2018-12-17 10:46:20 +0100

    Step 12 finished.

--- app/app.js
+++ app/app.js
@@ -6,23 +6,27 @@ myNinjaApp.controller('myNinjaController', ['$scope', function($scope){
     {
       name: "Yoshi",
       belt: "green",
-      rate: 50
+      rate: 50,
+      available: true
     },
     {
       name: "Crytstal",
       belt: "green",
-      rate: 30
+      rate: 30,
+      available: true
 
     },
     {
       name: "Ryu",
       belt: "orange",
-      rate: 10
+      rate: 10,
+      available: false
     },
     {
       name: "Shaun",
       belt: "black",
-      rate: 51000
+      rate: 51000,
+      available: true
     }
   ];
 }]);
