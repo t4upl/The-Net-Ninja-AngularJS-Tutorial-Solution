1b59a127dc15aac13af360e667177a6f229d6c9a
Patryk D <tomr33d32@gmail.com>
2018-12-18 14:41:00 +0100

    Step 16 finished.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,18 @@
-var myNinjaApp = angular.module('myNinjaApp', []);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl:'views\\home.html'
+    })
+    .when('/directory', {
+      templateUrl:'views\\directory.html',
+      controller: 'myNinjaController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    })
+}]);
 
 myNinjaApp.controller('myNinjaController', ['$scope', function($scope){
   $scope.message = "Hello from controller";
