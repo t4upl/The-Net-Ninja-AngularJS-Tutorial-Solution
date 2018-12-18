45806d6a50b81192526ed1718232521165758ea1
Patryk D <tomr33d32@gmail.com>
2018-12-18 17:23:16 +0100

    Step 19 finished.

--- app/app.js
+++ app/app.js
@@ -24,6 +24,8 @@ myNinjaApp.directive('randomNinja', [function(){
       title: '='
     },
     templateUrl: 'views\\random.html',
+    transclude: true,
+    replace: true,
     controller: function($scope){
       $scope.random = Math.floor(Math.random() * 4);
 
