1504dd337193af92bff50e601a09f5a7e2a65147
Patryk D <tomr33d32@gmail.com>
2018-12-21 10:14:57 +0100

    Step 22 finsihed.

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ myNinjaApp.config(['$routeProvider', function($routeProvider) {
       templateUrl:'views\\home.html',
       controller: 'myNinjaController'
     })
+    .when('/contact', {
+      templateUrl:'views\\contact.html'
+    })
     .when('/directory', {
       templateUrl:'views\\directory.html',
       controller: 'myNinjaController'
